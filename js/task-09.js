d7d5e70b8d3642cc534da732525a0e30fde02386
DianaRbk <robakdiana@gmail.com>
2023-05-11 23:27:45 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const colorButton = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+const body = document.body;
+
+colorButton.addEventListener('click', changeColor);
+
+function changeColor() {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+}
+
