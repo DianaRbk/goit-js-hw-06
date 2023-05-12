7d981051615f5eaa67242507c2543e346f508cbe
DianaRbk <robakdiana@gmail.com>
2023-05-12 22:21:06 +0300

    task-1_update

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,9 @@
 const categoriesList = document.querySelectorAll('ul#categories li.item');
 console.log('Number of categories:', categoriesList.length);
 
-categoriesList.forEach((category) => {
-  const categoryName = category.querySelector('h2').textContent;
-  const elements = category.querySelectorAll('li').length;
+Array.from(categoriesList).forEach((category) => {
+  const categoryName = category.firstElementChild.textContent;
+  const elements = category.getElementsByTagName('li').length;
   console.log(`\nCategory: ${categoryName}`);
   console.log(`Elements: ${elements}`);
 });
